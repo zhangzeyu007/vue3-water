327699574d6ca350adb3200223bbe53248d5af94
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-12 14:33:10 +0800

    feat:init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Author: 张泽雨
+ * @Date: 2024-03-04 19:20:38
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-12 14:17:10
+ * @FilePath: \vue3-warter\src\App.vue
+-->
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+.nav {
+  display: flex;
+  .book {
+    display: inline-block;
+  }
+}
+</style>
