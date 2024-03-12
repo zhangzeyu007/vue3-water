cf17a015ee6e67c1cc718c3659c462e4c3643324
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-12 20:48:28 +0800

    ff

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
  * @Author: 张泽雨
  * @Date: 2024-03-04 19:20:38
  * @LastEditors: 张泽雨
- * @LastEditTime: 2024-03-12 17:43:26
+ * @LastEditTime: 2024-03-12 20:31:41
  * @FilePath: \vue3-water\src\App.vue
 -->
 <script setup>
@@ -18,11 +18,11 @@ import { RouterView } from "vue-router";
 .nav {
   display: flex;
   .book {
-    color: #000;
-    background-color: #f2f2f2;
+    display: inline-block;
     padding: 10px;
     margin: 10px;
-    display: inline-block;
+    color: #000000;
+    background-color: #f2f2f2;
   }
 }
 </style>
